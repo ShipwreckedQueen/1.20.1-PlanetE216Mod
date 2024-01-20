bfa1178c8d578ecf794b0cb1265a1fcc6b59f5c2
ShipwreckedQueen <oflemir0colenoras@gmail.com>
2024-01-19 22:21:36 -0600

    Added some ai to Dodo entity, not sure how other animations work but I have an idea started

--- src/main/java/net/shipwreckfantasy/planete216/entity/custom/RaphusCucullatusEntity.java
+++ src/main/java/net/shipwreckfantasy/planete216/entity/custom/RaphusCucullatusEntity.java
@@ -2,16 +2,20 @@ package net.shipwreckfantasy.planete216.entity.custom;
 
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.behavior.FollowTemptation;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import net.shipwreckfantasy.planete216.entity.ModEntities;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
@@ -26,6 +30,11 @@ import software.bernie.geckolib.core.object.PlayState;
 
 public class RaphusCucullatusEntity extends Animal implements GeoEntity {
     private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+    private Object oFlap;
+    private Object flap;
+    private float flapSpeed;
+    private float oFlapSpeed;
+    private float flapping;
 
 
     public RaphusCucullatusEntity(EntityType<? extends Animal> pEntityType, Level pLevel) {
@@ -37,7 +46,10 @@ public class RaphusCucullatusEntity extends Animal implements GeoEntity {
         this.goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this,1.1D));
         this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 3f));
         this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
-        this.goalSelector.addGoal(1,new BreedGoal(this,1.15D));
+        this.goalSelector.addGoal(1,new BreedGoal(this,1.13D));
+        this.goalSelector.addGoal(1, new PanicGoal(this, 1.4D));
+        this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.1D));
+        this.goalSelector.addGoal(4, new TemptGoal(this,1.13D, Ingredient.of(Items.SWEET_BERRIES),false));
     }
 
 
@@ -46,6 +58,7 @@ public class RaphusCucullatusEntity extends Animal implements GeoEntity {
                 .add(Attributes.MAX_HEALTH, 4.0D)
                 .add(Attributes.MOVEMENT_SPEED, 0.13D)
                 .add(Attributes.FOLLOW_RANGE, 10D);
+
     }
 
 
@@ -84,6 +97,23 @@ public class RaphusCucullatusEntity extends Animal implements GeoEntity {
     @Override
     protected SoundEvent getHurtSound(DamageSource pDamageSource) {
         return super.getHurtSound(pDamageSource);
+    }
+
+    public void aiStep() {
+        super.aiStep();
+        this.oFlap = this.flap;
+        this.oFlapSpeed = this.flapSpeed;
+        this.flapSpeed += (this.onGround() ? -1.0F : 4.0F) * 0.3F;
+        this.flapSpeed = Mth.clamp(this.flapSpeed, 0.0F, 1.0F);
+        if (!this.onGround() && this.flapping < 1.0F) {
+            this.flapping = 1.0F;
+        }
+
+        this.flapping *= 0.9F;
+        Vec3 vec3 = this.getDeltaMovement();
+        if (!this.onGround() && vec3.y < 0.0D) {
+            this.setDeltaMovement(vec3.multiply(1.0D, 0.6D, 1.0D));
+        }
     }
 }
 
